ff5a22a4d2a7dce2c9b07085a3f983200420e56e
yoshi1220 <temari@themis.ocn.ne.jp>
2019-01-30 22:06:00 +0900

    Add home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home_controller/index'
+
   get 'reservations/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
