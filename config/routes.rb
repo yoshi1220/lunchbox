c229dd949f2049bbfb9b1e30690959cd8ae9adad
yoshi1220 <temari@themis.ocn.ne.jp>
2019-02-02 17:09:51 +0900

    Improve custom.scss, _header.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
-
-  get 'home_controller/index'
-
   get 'reservations/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
