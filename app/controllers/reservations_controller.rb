71c3b76998d042518d3dbb66fde35cc13c63d9ab
yoshi1220 <temari@themis.ocn.ne.jp>
2019-02-11 22:32:28 +0900

    Implement reservations_controller/create method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,4 +3,22 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
     @lunch_boxes = LunchBox.all
   end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = 1
+    @reservation.reserved = true
+    if @reservation.save
+      flash[:success] = 'お弁当の予約が完了しました。'
+      redirect_to :root
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reservation_date, :lunch_box_id)
+  end
 end
