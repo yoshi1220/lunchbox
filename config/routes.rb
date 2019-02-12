980c22a94cda24c776d8348a98d1a1c858e2964e
yoshi1220 <temari@themis.ocn.ne.jp>
2019-02-12 23:49:18 +0900

    新規ユーザー登録ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   resources :reservations
+  namespace :user do
+    resources :registrations, only: [:new, :create]
+  end
 end
