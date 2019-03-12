73fc85d24d48323cbe929f052ab794f77a5d1993
yoshi1220 <temari@themis.ocn.ne.jp>
2019-03-13 08:04:30 +0900

    _header.html.erbの改善。ログイン認証の改善

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :require_login
+
   def index
     @reservations = Reservation.where('reservation_date >= ?', Date.today) # ログインを実装するまで便宜的に1とする
   end
