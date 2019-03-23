dfe901124d5c780513a8e2cd2a1d900962923e9d
yoshi1220 <temari@themis.ocn.ne.jp>
2019-03-23 23:15:09 +0900

    デザインとコメントの微調整

--- app/services/reservation_service.rb
+++ app/services/reservation_service.rb
@@ -1,12 +1,14 @@
 module ReservationService
   module_function
 
+  # 当日の予約が存在しないかチェック
   def reservation_exists(reservation)
     Reservation.where(user_id: reservation.user_id)
                .where('reservation_date = ?', reservation.reservation_date)
                .first
   end
 
+  # 過去日付の予約かチェック
   def is_past_date(reservation_date)
     reservation_date < Date.today
   end
